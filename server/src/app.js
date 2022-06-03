eee966dc6951cfe5f377bed8a197a7a66c0d5d99
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-06-04 08:33:43 +1200

    create a mongodb server endpoint, create model for restaurants and begin working on first get endpoint for restaurants

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const mongoose = require("mongoose");
+const { celebrate, Joi, errors, Segments } = require("celebrate");
+const RestaurantModel = require("./models/RestaurantModel");
 app.use(cors());
 app.use(express.json());
+
+app.get("/", async (request, response) => {
+  response.send("Home Page: Please type a valid endpoints to receive data");
+  // does it need the async //
+});
+
+app.get("/restaurants", async (request, response) => {
+  const restaurants = await RestaurantModel.find();
+  if (restaurants.length > 0) {
+    return response.status(200).json(restaurants);
+  } else {
+    return response
+      .status(200)
+      .json({ message: "There are no saved restaurants yet" });
+  }
+});
 
 module.exports = app;
