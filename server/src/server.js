867d5db24abd3e6eb8bd551bf6cf6027eb4bd66d
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-06-16 12:10:23 +1200

    trying to get server to work on heroku

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,6 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const mongoDbUri = process.env.MONGO_URI || "mongodb://localhost:27017/mongo";
-
 mongoose.connect(mongoDbUri);
 
 app.listen(port, () => {
